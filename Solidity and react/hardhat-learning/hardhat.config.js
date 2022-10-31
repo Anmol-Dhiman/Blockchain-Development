1ededaab337fc5cff82f0c3749e8e2b608f74928
Anmol-Dhiman <anmoldhiman7111@gmail.com>
2022-11-01 00:40:35 +0530

    deploying on other networks

--- Solidity and react/hardhat-learning/hardhat.config.js
+++ Solidity and react/hardhat-learning/hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const rinkeby_url = process.env.RINKEBY_RPC_URL;
+const private_key = process.env.PRIVATE_KEY;
+
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    rinkeby: {
+      url: rinkeby_url,
+      accounts: [private_key],
+      chainId: 4,
+    },
+  },
   solidity: "0.8.17",
 };
