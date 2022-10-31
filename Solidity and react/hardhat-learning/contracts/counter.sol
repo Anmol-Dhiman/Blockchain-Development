e715ec855e8b38a41348437bac6b9f406ba95eb8
Anmol-Dhiman <anmoldhiman7111@gmail.com>
2022-11-01 00:26:25 +0530

    deploye contract using hardhat

---
+++ Solidity and react/hardhat-learning/contracts/counter.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+contract counter{
+
+uint private count;
+
+event incrementing(uint num);
+event decrementing(uint num);
+
+function increment() public{
+    count++;    
+    emit incrementing(count);
+
+}
+ function decrement() public{
+     count--;
+     emit decrementing(count);
+ }
+
+// constructor
+    constructor() {
+        count = 0;
+    }
+    function getCount() view public returns (uint){
+        return count;
+    }
+}
