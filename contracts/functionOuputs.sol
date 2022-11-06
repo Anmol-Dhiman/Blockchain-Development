efb53a8283dab3c3ee69a1b9f039009efd050803
Anmol-Dhiman <anmoldhiman7111@gmail.com>
2022-11-06 14:39:35 +0530

    multiple outputs

---
+++ contracts/functionOuputs.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Outputs {
+    // 21543
+    function simpleReturn() public pure returns (uint, bool) {
+        return (1, true);
+    }
+
+    // 21521
+    function nameReturn() public pure returns (uint x, bool b) {
+        return (1, true);
+    }
+
+    // 21499
+    function valueReturn() public pure returns (uint x, bool b) {
+        x = 1;
+        b = true;
+    }
+
+    // 21734
+    // how to get the value of these methods which return multiple values
+    function destructuringAssignment()
+        external
+        pure
+        returns (
+            uint a,
+            bool b,
+            bool c
+        )
+    {
+        (a, b) = valueReturn();
+        (, c) = nameReturn();
+    }
+}
